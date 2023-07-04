17a4565c77ecd7d061ecf597fe56de4a40e4bb50
Evgenii Neumerzhitckii <sausageskin@gmail.com>
2023-07-04 14:37:51 +1000

    Run inline javaascript

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -1,25 +1,17 @@
-# Be sure to restart your server when you modify this file.
+Rails.application.configure do
+  # The policies are set in config/environments/[development/test/production].rb
 
-# Define an application-wide content security policy.
-# See the Securing Rails Applications Guide for more information:
-# https://guides.rubyonrails.org/security.html#content-security-policy-header
+  # Generate random nonce that will be used in <script> and <style> elements
+  config.content_security_policy_nonce_generator = -> (request) { SecureRandom.base64(16) }
 
-# Rails.application.configure do
-#   config.content_security_policy do |policy|
-#     policy.default_src :self, :https
-#     policy.font_src    :self, :https, :data
-#     policy.img_src     :self, :https, :data
-#     policy.object_src  :none
-#     policy.script_src  :self, :https
-#     policy.style_src   :self, :https
-#     # Specify URI for violation reports
-#     # policy.report_uri "/csp-violation-report-endpoint"
-#   end
-#
-#   # Generate session nonces for permitted importmap and inline scripts
-#   config.content_security_policy_nonce_generator = ->(request) { request.session.id.to_s }
-#   config.content_security_policy_nonce_directives = %w(script-src)
-#
-#   # Report violations without enforcing the policy.
-#   # config.content_security_policy_report_only = true
-# end
+  # Add the nonce to the Content-Security-Policy header
+  config.content_security_policy_nonce_directives = %w(script-src style-src)
+
+  config.content_security_policy do |policy|
+    policy.default_src :self
+    policy.script_src  :self
+    policy.style_src   :self
+    policy.connect_src :self
+  end
+end
+
